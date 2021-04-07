1ce7aa289dc8d6dac45f5d9f67619f9887c0fb50
230442 Michal Augustyniak <230442@edu.p.lodz.pl>
2021-04-07 23:06:36 +0200

    Added Data Layer

---
+++ LibraryTask1/LibraryTask1/DataLayer/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTask1.DataLayer
+{
+    class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int BookID { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Book(string name, string author, int bookID, bool isAvailable)
+        {
+            Name = name;
+            Author = author;
+            BookID = bookID;
+            IsAvailable = isAvailable;
+        }
+    }
+}
---
+++ LibraryTask1/LibraryTask1/DataLayer/Events.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTask1.DataLayer
+{
+    class Events
+    {
+        public Book BookItem { get; set; }
+        public Reader ReaderPerson { get; set; }
+        public DateTime Date { get; set; }
+
+        public Events(Book bookItem, Reader readerPerson, DateTime date)
+        {
+            BookItem = bookItem;
+            ReaderPerson = readerPerson;
+            Date = date;
+        }
+    }
+}
