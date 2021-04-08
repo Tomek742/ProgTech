2e5190e33ad839c37edae9bb96a449f930aa9aa3
223964 Tomasz Kolas <223964@edu.p.lodz.pl>
2021-04-08 19:26:15 +0200

    Added more logic and some contect to data

---
+++ LibraryTask1/LibraryTask1/LogicLayer/DataContects.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace LibraryTask1.LogicLayer
+{
+    class DataContects
+    {
+        public Dictionary<string, Book> BookList = new Dictionary<string, Book>();
+    }
+}
