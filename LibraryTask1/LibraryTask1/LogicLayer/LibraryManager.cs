d0ee5058dc7b90e7af97bbc29bdf04c17263009f
223964 Tomasz Kolas <223964@edu.p.lodz.pl>
2021-04-08 18:26:50 +0200

    Added some of the logic

--- LibraryTask1/LibraryTask1/LogicLayer/LibraryManager.cs
+++ LibraryTask1/LibraryTask1/LogicLayer/LibraryManager.cs
@@ -9,5 +9,27 @@ namespace LibraryTask1.LogicLayer
 {
     class LibraryManager
     {
+        public void AddBook(Book book)
+        {
+            if (book != null)
+                Content.Add(book);
+            else
+                throw new ArgumentNullException();
+        }
+        public void DestroyBook(Book book)
+        {
+            if (book != null)
+                Content.Remove(book);
+            else
+                throw new ArgumentNullException();
+        }
+        public void BorrowBook(Book book)
+        {
+            if (book != null)
+                book.IsAvailable.set(false);
+            else
+                throw new ArgumentNullException();
+        }
+
     }
 }
