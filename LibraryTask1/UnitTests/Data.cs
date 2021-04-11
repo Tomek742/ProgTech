e28b66f74c1dbb7628209d6639880a9c9e939196
223964 Tomasz Kolas <223964@edu.p.lodz.pl>
2021-04-11 23:02:29 +0200

    Final Constructor tests

--- LibraryTask1/UnitTests/Data.cs
+++ LibraryTask1/UnitTests/Data.cs
@@ -24,5 +24,27 @@ namespace UnitTests
             Assert.AreEqual(reader.Name, "Adam");
             Assert.AreEqual(reader.ReaderID, 1);
         }
+
+        [TestMethod]
+        public void EventConstructor()
+        {
+            DateTime sss = DateTime.Now;
+            Reader reader = new Reader("Adam", 1);
+            Book book = new Book("Pan Tadeusz", "Adam Mickiewicz", 1);
+            Event e = new Event(book, reader, sss, 1);
+            Assert.AreEqual(e.BookItem, book);
+            Assert.AreEqual(e.ReaderPerson, reader);
+            Assert.AreEqual(e.Date, sss);
+            Assert.AreEqual(e.EventID, 1);
+        }
+
+        [TestMethod]
+        public void ContentConstructor()
+        {
+            Book book = new Book("Pan Tadeusz", "Adam Mickiewicz", 1);
+            Content e = new Content(book, 1);
+            Assert.AreEqual(e.BookItem, book);
+            Assert.AreEqual(e.Quantity, 1);
+        }
     }
 }
