d848dacd430a42d77405e2ba70c161d4e530809b
223964 Tomasz Kolas <223964@edu.p.lodz.pl>
2021-05-09 17:03:20 +0200

    Made MainWindow

---
+++ LibraryTask2/Presentation/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Logic;
+
+namespace Presentation
+{
+    class ViewModel
+    {
+        public void AddReader()
+        {
+            AddReader();
+        }
+    }
+}
